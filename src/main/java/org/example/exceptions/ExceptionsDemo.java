97b7a60139a5dd506e0e7e6c853169ca7b96489b
Moonwar <nishan.rumc@gmail.com>
2024-10-08 13:13:12 +0600

    exception handled

--- src/main/java/org/example/exceptions/ExceptionsDemo.java
+++ src/main/java/org/example/exceptions/ExceptionsDemo.java
@@ -1,9 +1,21 @@
 package org.example.exceptions;
 
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+
 public class ExceptionsDemo {
     public static void show()
     {
-        sayHello(null);
+        try
+        {
+            var reader = new FileReader("file.txt");
+            System.out.println("File Opened");
+        }
+        catch (FileNotFoundException ex)
+        {
+            System.out.println(ex.getMessage());
+        }
+        //sayHello(null);
     }
     public static void sayHello(String name)
     {
