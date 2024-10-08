592e3aa294f946cd72d2070ea1f47be8b655d3a7
Moonwar <nishan.rumc@gmail.com>
2024-10-08 14:34:50 +0600

    Re-Throw an exception

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,8 +3,10 @@ package org.example;
 import org.example.exceptions.Account;
 import org.example.exceptions.ExceptionsDemo;
 
+import java.io.IOException;
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 
         //ExceptionsDemo.show();
         var account = new Account();
@@ -14,6 +16,7 @@ public class Main {
         catch (Exception e)
         {
             System.out.println(e.getMessage());
+            throw e;
         }
 
     }
