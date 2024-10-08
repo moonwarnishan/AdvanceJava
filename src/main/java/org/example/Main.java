79e547872072f95af7801490853b9915376a389f
Moonwar <nishan.rumc@gmail.com>
2024-10-08 15:32:45 +0600

    generic list

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,6 +4,7 @@ import org.example.exceptions.Account;
 import org.example.exceptions.AccountException;
 import org.example.exceptions.ExceptionsDemo;
 import org.example.exceptions.InsufficientFundException;
+import org.example.generics.GenericList;
 
 import java.io.IOException;
 
@@ -27,13 +28,17 @@ public class Main {
 //            e.printStackTrace();
 //        }
 
-        var account = new Account();
-        try {
-            account.withdraw(1000);
-        } catch (AccountException e) {
-            e.printStackTrace();
-            throw new RuntimeException(e);
-        }
+//        var account = new Account();
+//        try {
+//            account.withdraw(1000);
+//        } catch (AccountException e) {
+//            e.printStackTrace();
+//            throw new RuntimeException(e);
+//        }
 
+        var items = new GenericList<Integer>();
+        items.add(1);
+        int x = items.get(0);
+        System.out.println(x);
     }
 }
