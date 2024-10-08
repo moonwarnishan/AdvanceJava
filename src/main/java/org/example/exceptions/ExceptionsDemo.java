593c6628c10e86e1f6eb1f8c15e5cba28f66dc90
Moonwar <nishan.rumc@gmail.com>
2024-10-08 13:08:27 +0600

    create exception

--- src/main/java/org/example/exceptions/ExceptionsDemo.java
+++ src/main/java/org/example/exceptions/ExceptionsDemo.java
@@ -3,6 +3,10 @@ package org.example.exceptions;
 public class ExceptionsDemo {
     public static void show()
     {
-        
+        sayHello(null);
+    }
+    public static void sayHello(String name)
+    {
+        System.out.println(name.toUpperCase());
     }
 }
